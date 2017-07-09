969024084ed9d4fd1edc61bf1dd003349a3d11f2
AshaElesabeth <ashaelesabethj@gmail.com>
2017-07-09 11:07:02 +0530

    Created User model and used angular.js for routing and actions, given by SailsinACtion

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -6,20 +6,24 @@
  * This gives you an opportunity to set up your data model, run jobs, or perform some special logic.
  *
  * For more information on bootstrapping your app, check out:
- * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
+ * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
 module.exports.bootstrap = function(cb) {
 
+  // Return the number of records in the video model
   Video.count().exec(function(err, numVideos) {
     if (err) {
       return cb(err);
     }
 
+    // If there's at least one log the number to the console.
     if (numVideos > 0) {
       console.log('Existing video records: ', numVideos)
       return cb();
     }
+
+    // Add machinepack-youtube as a depedency
     var Youtube = require('machinepack-youtube');
 
     // List Youtube videos which match the specified search query.
@@ -30,32 +34,25 @@ module.exports.bootstrap = function(cb) {
     }).exec({
       // An unexpected error occurred.
       error: function(err) {
-        console.log('an error: ', err);
-        return cb(err);
 
       },
       // OK.
       success: function(foundVideos) {
-        console.log('the foundVideos: ', foundVideos);
-
-        _.each(foundVideos, function(video) { 
-          video.src = 'https://www.youtube.com/embed/' + video.id; 
-          delete video.description; 
-          delete video.publishedAt; 
-          delete video.id; 
-          delete video.url; 
+        _.each(foundVideos, function(video) {
+          video.src = 'https://www.youtube.com/embed/' + video.id;
+          delete video.description;
+          delete video.publishedAt;
+          delete video.id;
+          delete video.url;
         });
 
-        // Add the transformed video records to the video model
         Video.create(foundVideos).exec(function(err, videoRecordsCreated) {
           if (err) {
             return cb(err);
           }
-
-          console.log(videoRecordsCreated); 
+          console.log(foundVideos);
           return cb();
         });
-
       },
     });
   });
